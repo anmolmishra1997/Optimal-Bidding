c9fceafee277253325a38c1d3bcf23230219905f
kumarashutosh.ee@gmail.com <kumarashutosh.ee@gmail.com>
2017-11-30 02:19:03 +0530

    solar and price added

---
+++ hist_Price.py
@@ -0,0 +1,42 @@
+from scipy.stats import norm
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import csv
+import pandas as pd
+import numpy as np
+
+
+# read data from a text file. One number per line
+price_train = pd.read_csv('Price_Train.csv', header=None).as_matrix()
+price_train_pred = pd.read_csv('Price_Train_pred.csv', header=None).as_matrix()
+
+mu_total = []
+sigma_total = []
+
+for i in range(0,24):
+	datos = price_train[:,i] - price_train_pred[:,i]
+
+	# best fit of data
+	(mu, sigma) = norm.fit(datos)
+	mu_total.append(mu)
+	sigma_total.append(sigma)
+
+	# the histogram of the data
+	n, bins, patches = plt.hist(datos, 60, normed=1, facecolor='green', alpha=0.75)
+
+	# add a 'best fit' line
+	y = mlab.normpdf( bins, mu, sigma)
+	l = plt.plot(bins, y, 'r--', linewidth=2)
+
+	#plot
+	plt.xlabel('Smarts')
+	plt.ylabel('Probability')
+	plt.title(r'$\mathrm{Histogram\ of\ IQ:}\ \mu=%.3f,\ \sigma=%.3f$' %(mu, sigma))
+	plt.grid(True)
+
+	plt.show()
+
+x = open('data_obs_price.txt', 'w')
+for j in range(0,24):
+	x.write("Mean =" + str(mu_total[j]) + " Std dev = " + str(sigma_total[j]) + " \n ")
+x.close()
